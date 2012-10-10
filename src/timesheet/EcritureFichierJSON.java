3f9efa0708dc4aa4680a4080852dc7cc0f821050
SirZerator <sirzerator@sirzerator-VirtualBox.(none)>
2012-10-10 16:01:15 -0400

    Premier commit.

---
+++ src/timesheet/EcritureFichierJSON.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package timesheet;
+
+/**
+ *
+ * @author sirzerator
+ */
+public class EcritureFichierJSON {
+    
+}
