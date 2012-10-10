2646e68fcf23bc8b6da93b3f8e9e1ba9a6fd980b
FredSev <mobworld17@hotmail.com>
2012-10-10 18:44:05 -0400

    second commit

--- src/timesheet/Employe.java
+++ src/timesheet/Employe.java
@@ -1,9 +1,43 @@
 package timesheet;
 
-/**
- *
- * @author sirzerator
- */
 public class Employe {
+    
+    public void setNumeroEmploye(int numEmp){
+        numeroEmploye = numEmp;
+    }
+    
+    public void setTempsTravaille(int jours, int project, int minutes){
+        if(project > 900){
+            totalTempsTele += minutes;
+        }else{
+            totalTempsBureau += minutes;
+            tempsBureauParJours[jours] += minutes;
+        }
+    }
+    
+    public int getNumeroEmploye(){
+        return numeroEmploye;
+    }
+    
+    public int getTotalTempsTele(){
+        return totalTempsTele;
+    }
+    
+    public int getTotalTempsBureau(){
+        return totalTempsBureau;
+    }
+    
+    public int getTempsUnJour(int jour){
+        return tempsBureauParJours[jour];
+    }
+    
+    public int[] getTempsBureauParJours(){
+        return tempsBureauParJours;
+    }
+    
+    private int numeroEmploye;
+    private int totalTempsTele;
+    private int totalTempsBureau;
+    private int tempsBureauParJours[] = {0,0,0,0,0,0,0};
     
 }
